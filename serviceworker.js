18cfa65350fc366e3e42990b06999e0c30063504
dsx-html <151399632+dsx-html@users.noreply.github.com>
2023-12-02 04:52:18 +0300

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,29 +1,29 @@
 
-const CACHE_NAME = 'dsx.html_v1.25_(chinese_full)';
+const CACHE_NAME = 'dsx.html_v1.3_chinese_lite';
 
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/index.html'
-    ]);
+    try {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll([
+        '/index.html'
+      ]);
+    } catch (er) {}
   })());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith((async () => {
     const cache = await caches.open(CACHE_NAME);
-
     const cachedResponse = await cache.match(event.request);
     if (cachedResponse) {
       return cachedResponse;
     } else {
-        try {
-          const fetchResponse = await fetch(event.request);
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (er) {
-        }
+      try {
+        const fetchResponse = await fetch(event.request);
+        cache.put(event.request, fetchResponse.clone());
+        return fetchResponse;
+      } catch (er) {}
     }
   })());
 });
